e8ee4740362d9c86fd9d6fefd41b0f693c0c0524
IR1020 <biz.ir1020@gmail.com>
2021-02-12 17:24:53 +0900

    test commit

--- resources/views/touroku.blade.php
+++ resources/views/touroku.blade.php
@@ -2,20 +2,18 @@
 <html lang="ja">
 
 <head>
-<meta charset="UTF-8">
-<title>新規登録画面</title>
+    <meta charset="UTF-8">
+    <title>新規登録画面</title>
 </head>
-
 <body>
-<h1>SNS名(新規登録画面)</h1>
-<p>ユーザー登録を行います。<br>
-ご希望のユーザー名とパスワードを入力してください。</p>
-
-<form action="touroku" method="post">
-@csrf
-<p>ユーザー名:<input type="text" name="name"></p>
-<p>パスワード:<input type="password" name="pass"></p>
-<p><input type="submit" value="登録"><br></p>
-</form>
+    <h1>SNS名(新規登録画面)</h1>
+    <p>ユーザー登録を行います。<br>
+    ご希望のユーザー名とパスワードを入力してください。</p>
+    <form action="touroku" method="post">
+        @csrf
+        <p>ユーザー名:<input type="text" name="name"></p>
+        <p>パスワード:<input type="password" name="pass"></p>
+        <p><input type="submit" value="登録"><br></p>
+    </form>
 </body>
 </html>
